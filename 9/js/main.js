71024eafb5da04a02bcab1cc7121e22ce13991df
Keks <keks@htmlacademy.ru>
2022-12-20 08:00:00 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,9 @@
+import {photos} from './data.js';
+import {renderPhotos} from './pictures.js';
+import './formUpload.js';
+import './showBigPictures.js';
+import './hashtags.js';
+import './scalingPhoto.js';
+import './effectsOnPhoto.js';
+
+renderPhotos(photos);
